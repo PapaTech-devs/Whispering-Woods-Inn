53f3e681a9c94824534cdd72a2a455bf2c0d28b7
dhruvroytalukdar <dhruvroy8@gmail.com>
2022-09-08 17:39:16 +0530

    feat: payment gateway implemented

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1,23 +1,141 @@
 import "./Payment.css"
+import { useState } from "react"
+import { Link } from "react-scroll"
+import displayRazorpay from "../../utils/displayRazorPay"
 
 const Payment = () => {
+  const priceOfRooms = {
+    acroom: 350,
+    nonacroom: 250,
+    accottage: 500,
+  }
+
+  const [values, setValues] = useState({
+    room: "nonacroom",
+    name: "",
+    email: "",
+    phoneNumber: "",
+    startDate: "",
+    endDate: "",
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setValues({ ...values, [name]: value })
+  }
+
+  const validateEmail = (email) => {
+    // eslint-disable-next-line
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+      email.toLowerCase()
+    )
+  }
+
+  const getDateDiff = () => {
+    const diffTime = Math.abs(
+      new Date(values.endDate) - new Date(values.startDate)
+    )
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+    return diffDays
+  }
+
+  const validatePhoneNumber = (phone) => {
+    return /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/.test(phone)
+  }
+
+  const handlePay = async () => {
+    if (values.email === "" || !validateEmail(values.email)) {
+      alert("Please give a valid email")
+      return
+    }
+
+    if (values.phoneNumber === "" || !validatePhoneNumber(values.phoneNumber)) {
+      alert("Please give a valid phone number")
+      return
+    }
+
+    if (values.name === "") {
+      alert("Please give your full name")
+      return
+    }
+
+    if (values.startDate === "") {
+      alert("Please give a valid start date")
+      return
+    }
+
+    if (values.endDate === "") {
+      alert("Please select a valid end date")
+      return
+    }
+
+    if (new Date(values.endDate) <= new Date(values.startDate)) {
+      alert("Invalid date input")
+      return
+    }
+
+    await displayRazorpay(
+      priceOfRooms[values.room] * getDateDiff(),
+      values.name,
+      values.email,
+      values.phoneNumber,
+      values.startDate,
+      values.endDate,
+      values.room
+    )
+  }
+
   return (
     <div className="main" id="payment">
       <p className="title">Book a room</p>
+      <h2>Instructions</h2>
+      <ol>
+        <li>
+          First{" "}
+          <Link to="contact" spy={true} smooth={true}>
+            call
+          </Link>{" "}
+          the hotel to check room availability
+        </li>
+        <li>Pay advance booking fees</li>
+        <li>
+          You will get a booking id which you need to show at hotel reception
+        </li>
+      </ol>
       <div className="payment-content">
         <div className="personal">
-          <input type="text" placeholder="Your name" />
-          <input type="text" placeholder="Your email" />
-          <input type="text" placeholder="Your mobile number" />
+          <input
+            onChange={handleInputChange}
+            name="name"
+            type="text"
+            placeholder="Your name"
+          />
+          <input
+            onChange={handleInputChange}
+            name="email"
+            type="text"
+            placeholder="Your email"
+          />
+          <input
+            onChange={handleInputChange}
+            name="phoneNumber"
+            type="text"
+            placeholder="Your mobile number"
+          />
         </div>
         <div className="hotel-info">
           <div className="inputs">
             <label htmlFor="select" style={{ marginRight: "10px" }}>
               Select type of Room
             </label>
-            <select id="select">
-              <option value="acroom">Non AC Room</option>
-              <option value="nonacroom">AC Room</option>
+            <select
+              id="select"
+              value={values.room}
+              name="room"
+              onChange={handleInputChange}
+            >
+              <option value="nonacroom">Non AC Room</option>
+              <option value="acroom">AC Room</option>
               <option value="accottage">AC Cottage</option>
             </select>
           </div>
@@ -25,19 +143,37 @@ const Payment = () => {
             <label htmlFor="FromDate" style={{ marginRight: "10px" }}>
               Select start of stay
             </label>
-            <input type="date" id="FromDate" />
+            <input
+              onChange={handleInputChange}
+              name="startDate"
+              type="date"
+              id="FromDate"
+            />
           </div>
           <div className="inputs">
             <label htmlFor="ToDate" style={{ marginRight: "10px" }}>
               Select end of stay
             </label>
-            <input type="date" placeholder="ToDate" />
+            <input
+              onChange={handleInputChange}
+              name="endDate"
+              type="date"
+              placeholder="ToDate"
+            />
           </div>
         </div>
       </div>
       <div className="amount-section">
-        <button className="button pay-button">Pay</button>
-        <p className="price-text">₹500 per night</p>
+        <button className="button pay-button" onClick={handlePay}>
+          Pay
+        </button>
+        <p className="price-text">
+          ₹{priceOfRooms[values.room]}/night{" "}
+          {values.endDate !== "" &&
+            values.startDate !== "" &&
+            new Date(values.endDate) > new Date(values.startDate) &&
+            `= ₹${priceOfRooms[values.room] * getDateDiff()}`}
+        </p>
       </div>
     </div>
   )
---
+++ src/utils/displayRazorPay.js
@@ -0,0 +1,84 @@
+import axios from "axios"
+import loadScript from "./loadScript"
+
+async function displayRazorpay(
+  priceHotel,
+  userName,
+  userEmail,
+  userPhoneNumber,
+  startDate,
+  toDate,
+  room
+) {
+  const res = await loadScript("https://checkout.razorpay.com/v1/checkout.js")
+
+  if (!res) {
+    alert("Razorpay SDK failed to load. Are you online?")
+    return
+  }
+
+  // creating a new order
+  const result = await axios.post(
+    `${process.env.REACT_APP_BACKEND_URL}/payment/orders`,
+    {
+      amount: priceHotel,
+    }
+  )
+
+  if (!result) {
+    alert("Server error. Are you online?")
+    return
+  }
+
+  // Getting the order details back
+  const { amount, id: order_id, currency } = result.data
+
+  const options = {
+    key: process.env.REACT_APP_RAZORKEY, // Enter the Key ID generated from the Dashboard
+    amount: amount.toString(),
+    currency: currency,
+    name: "Whispering Woods",
+    description: `Transaction by ${userName}`,
+    // image: { logo },
+    order_id: order_id,
+    handler: async function (response) {
+      const data = {
+        orderCreationId: order_id,
+        razorpayPaymentId: response.razorpay_payment_id,
+        razorpayOrderId: response.razorpay_order_id,
+        razorpaySignature: response.razorpay_signature,
+        user: userName,
+        email: userEmail,
+        phone: userPhoneNumber,
+        fromDate: startDate,
+        toDate: toDate,
+        amount: amount,
+        roomType: room,
+      }
+
+      try {
+        const result = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}/payment/success`,
+          data
+        )
+        alert(
+          `PLEASE STORE PAYMENTID = ${result.data.paymentId} ORDERID = ${result.data.orderId}`
+        )
+      } catch (err) {
+        console.log("Error", err)
+      }
+    },
+    prefill: {
+      name: userName,
+      contact: userPhoneNumber,
+    },
+    theme: {
+      color: "#FCA61F",
+    },
+  }
+
+  const paymentObject = new window.Razorpay(options)
+  paymentObject.open()
+}
+
+export default displayRazorpay
